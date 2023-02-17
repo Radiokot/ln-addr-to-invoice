dc7d670a41186664c6a7121932168402da52fb8c
Oleg Koretsky <oleg@radiokot.com.ua>
2023-02-17 11:53:18 +0200

    Initialize the project

---
+++ app/src/main/java/ua/com/radiokot/lnaddr2invoice/MainActivity.kt
@@ -0,0 +1,26 @@
+package ua.com.radiokot.lnaddr2invoice
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import com.google.android.material.snackbar.Snackbar
+import ua.com.radiokot.lnaddr2invoice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAnchorView(R.id.fab)
+                .setAction("Action", null).show()
+        }
+    }
+}
